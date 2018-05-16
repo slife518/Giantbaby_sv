42bd0548461f8b496d05074065c7f8cea0cda7d6
조정국 <slife518@gmail.com>
2018-05-16 17:27:40 +0900

    codeIgniter 반영

---
+++ application/models/topic_model.php
@@ -0,0 +1,18 @@
+<?php
+class Topic_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function gets(){
+        return $this->db->query("SELECT * FROM topic")->result();   //result_array 로도 가능
+    }
+
+    function get($topic_id){
+        return $this->db->get_where('topic', array('id'=>$topic_id))->row();
+    }
+}
+
+?>
