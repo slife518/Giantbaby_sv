689ca803cc6912699fa6a853ec1091494566149a
JK <slife518@gmail.com>
2018-09-04 17:37:39 +0900

    ..

--- application/controllers/Pc_Baby.php
+++ application/controllers/Pc_Baby.php
@@ -28,9 +28,47 @@ class Pc_Baby extends My_Controller {
       $baby_id = $this->input->post('baby_id');
       log_message('debug', $baby_id);      
 
-      $result = $this->pc_baby_model->getbabydetail(array("email"=>$email,"baby_id"=>$baby_id));
+      $result = $this->pc_baby_model->getbabydetail(array("owner"=>$email,"baby_id"=>$baby_id));
       log_message('debug',print_r($result,TRUE));
       echo json_encode(array("result"=>$result),JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+    }
+
+
+    function registerBaby(){
+      log_message('debug','register 시작');
+      log_message('debug',$this->input->post('email'));
+              $array = array(
+                    'owner'=>$this->input->post('email'),
+                    'babyname'=>$this->input->post('babyname'),
+                    'birthday'=>$this->input->post('birthday'),
+                    'mother'=>$this->input->post('mother'),
+                    'father'=>$this->input->post('father'),
+                    'sex'=>$this->input->post('sex')
+                  );
+            log_message('debug',print_r($array, TRUE));
+               $baby_id = $this->pc_baby_model->registerBaby($array);
+               array_merge($array, array('baby_id'=>$baby_id));
+               echo json_encode($array);  //json 형식으로 보내고 json 을 받아서 화면에서 배열로 세팅한다.
+    }
+
+
+    function update(){
+      log_message('debug', '아기정보수정');
+    //  $this->load->library('form_validation');
+      log_message('debug', '아기이름은 ' . $this->input->post('babyname')  );
+          log_message('debug', '아기정보저장 중')   ;
+          log_message('debug', $this->input->post('email'));
+            $array = array(
+                    'baby_id'=>$this->input->post('baby_id'),
+                    'mother'=>$this->input->post('mother'),
+                    'father'=>$this->input->post('father'),
+                    'babyname'=>$this->input->post('babyname'),
+                    'sex'=>$this->input->post('sex'),
+                    'birthday'=>$this->input->post('birthday')
+                    );
+            log_message('debug', print_r($array, 'TRUE'));
+            $this->pc_baby_model->update($array);                        
+            echo json_encode('아기정보가 수정되었습니다.');
     }
 }
 ?>
